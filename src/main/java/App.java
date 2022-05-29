e1982a2d39b3bd11ae6b76fb923f5cab517da658
Jakub <jakub.dragosz@o2.pl>
2022-05-29 15:25:05 +0200

    dla klienta w pytke, mozesz zaczynac

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,10 @@
+import static spark.Spark.port;
+import static spark.Spark.*;
 
 public class App {
+    public static void main(String[] args) {
+        staticFiles.location("/public");
+        port(3000);
+        get("/test", (req, res) -> "test");
+    }
 }
